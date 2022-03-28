77711db18a741f0e23876eac6c788d4d357226bf
Joff Tiquez <jofftiquez@gmail.com>
2022-03-28 16:18:53 +0800

    chore: add sample config

---
+++ config/custom.js
@@ -0,0 +1,12 @@
+const APP_NAME = 'Custom';
+const APP_DESCRIPTION = 'Sit duis veniam ullamco non exercitation anim dolore est eiusmod ipsum.';
+
+module.exports = {
+  name: APP_NAME,
+  quasarConfig: {
+    htmlVariables: {
+      title: APP_NAME,
+      description: APP_DESCRIPTION,
+    },
+  },
+};
---
+++ config/default.js
@@ -0,0 +1,12 @@
+const APP_NAME = 'Default';
+const APP_DESCRIPTION = 'Sit ipsum nostrud sit enim in quis in labore ut.';
+
+module.exports = {
+  name: APP_NAME,
+  quasarConfig: {
+    htmlVariables: {
+      title: APP_NAME,
+      description: APP_DESCRIPTION,
+    },
+  },
+};
