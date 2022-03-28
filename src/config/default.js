448e25e3ea117a5e6cab01c9107019c90a33d155
Joff Tiquez <jofftiquez@gmail.com>
2022-03-28 18:00:28 +0800

    chore: add favicon

--- src/config/default.js
+++
@@ -1,8 +0,0 @@
-export default {
-  appName: 'MYCURE CMS',
-  description: 'Ut dolore anim sunt dolor excepteur deserunt aute excepteur.',
-  htmlVariables: {
-    title: 'MYCURE CMS',
-    description: 'Et deserunt do elit sunt.',
-  },
-};
