5bd52bfdc7b42e98ccb26c41f1072bf6fcd7a110
Joff Tiquez <jofftiquez@gmail.com>
2022-04-10 17:18:07 +0800

    refactor: add generic page component

---
+++ src/components/commons/GenericPage.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+q-page(:padding="padding")
+  slot(name="default")
+  q-page-sticky(position="bottom-right" :offset="[18, 18]")
+    slot(name="fab")
+  q-dialog(
+    v-model="loading"
+    full-screen
+    persistent
+  )
+    q-spinner(
+      size="50px"
+      color="white"
+    )
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  setup () {
+    const padding = ref(false);
+    const loading = ref(false);
+
+    return {
+      padding,
+      loading,
+    };
+  },
+};
+</script>
