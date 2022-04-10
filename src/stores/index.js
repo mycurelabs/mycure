8301ca73e83f5ca6f5dd3e2a582e41d68fc57b6b
Joff Tiquez <jofftiquez@gmail.com>
2022-04-11 02:13:04 +0800

    refactor: add persisteState module

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,5 +1,6 @@
 import { store } from 'quasar/wrappers';
 import { createPinia } from 'pinia';
+import persistedstate from 'pinia-persistedstate';
 
 /*
  * If not building with SSR mode, you can
@@ -14,7 +15,10 @@ export default store((/* { ssrContext } */) => {
   const pinia = createPinia();
 
   // You can add Pinia plugins here
-  // pinia.use(SomePiniaPlugin)
+  pinia.use(persistedstate({
+    key: 'client',
+    // paths: ['dataStore', 'dataStore.count']  // Keep state, use module id, or state
+  }));
 
   return pinia;
 });
