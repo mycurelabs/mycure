054cfd6461791c10e5522e0dd5a3ea5a28228862
Joff Tiquez <jofftiquez@gmail.com>
2023-02-16 14:47:22 +0800

    refactor: update env

--- src/boot/mycure.js
+++ src/boot/mycure.js
@@ -3,7 +3,7 @@ import { boot } from 'quasar/wrappers';
 import sdk from '@mycure/sdk-js';
 
 export default boot(({ app }) => {
-  const apiServerURI = process.env.API_URL;
+  const apiServerURI = process.env.API;
   if (apiServerURI) {
     sdk.initialize(apiServerURI, {
       timeout: 30000,
