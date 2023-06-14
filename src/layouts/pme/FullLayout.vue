32ec1cdfdc6d34b528bc1b0d8e9dfba0bbc91f84
Joff Tiquez <jofftiquez@gmail.com>
2023-06-15 01:29:44 +0800

    🎨 style(FullLayout.vue): adjust layout and styling of FullLayout component The layout and styling of the FullLayout component have been adjusted. The q-layout view property has been changed to "lHh LpR lFr" to better fit the design requirements. The q-header component has been given a dark border and the q-toolbar component has been given a background color of #00597a to match the design.
    🎨 style(FullLayout.vue): improve layout and styling of the FullLayout component
    The changes include:
    - Adding the current page title to the toolbar title
    - Adding a user avatar and dropdown menu with a logout option
    - Changing the left drawer to be dark and show only when above a certain size
    - Improving the styling of the active clinic item in the left drawer
    - Adding a dark background to the left drawer and separating the items with a dark separator.
    
    🎨 style(FullLayout.vue): add computed properties for current user's full name and profile picture URL
    ✨ feat(FullLayout.vue): add logout function
    The computed properties `fullName` and `currentUserPicURL` are added to display the current user's full name and profile picture URL respectively. The `logOut` function is added to allow the user to log out of the application. The function displays a confirmation dialog before calling the `sdk.service('auth').signout()` function to log the user out.

--- src/layouts/pme/FullLayout.vue
+++ src/layouts/pme/FullLayout.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
-q-layout(view="hHh LpR lFr" style="background: #fafafa")
-  q-header(bordered).text-black.bg-white
-    q-toolbar(style="height: 60px")
+q-layout(view="lHh LpR lFr" style="background: #fafafa")
+  q-header(bordered dark)
+    q-toolbar(style="height: 60px; background: #00597a")
       q-btn(
         aria-label="Menu",
         icon="las la-bars",
@@ -9,28 +9,59 @@ q-layout(view="hHh LpR lFr" style="background: #fafafa")
         round
         @click="toggleLeftDrawer"
       )
-      q-toolbar-title MYCURE - PME
+      q-toolbar-title PME - {{$route.meta.pageTitle}}
+      q-space
+      q-btn(
+        flat
+        round
+      )
+        q-avatar
+          q-img(:src="currentUserPicURL")
+        q-menu(
+          anchor="bottom right"
+          self="top right"
+        )
+          q-list(style="min-width: 100px")
+            q-item(v-close-popup)
+              q-item-section
+                q-item-label Welcome, {{fullName}}
+            q-separator
+            q-item(v-close-popup clickable @click="logOut")
+              q-item-section
+                q-item-label Logout
 
-  q-drawer(v-model="leftDrawerOpen", bordered)
-    q-list
-      //- q-item
-        q-item-label(label).text-grey
-          small.text-bold Active Clinic
+  q-drawer(
+    v-model="leftDrawerOpen"
+    bordered
+    dark
+    show-if-above
+    mini-to-overlay
+    :mini="miniState"
+    @mouseover="miniState = false"
+    @mouseout="miniState = true"
+    style="background: #142731"
+  )
+    q-toolbar(style="height: 60px").q-pa-0
+      img(v-if="miniState" src="../../assets/images/logo-icon-white.png" style="height: 35px; margin-left: -3px")
+      img(v-else src="../../assets/images/logo-text-white.png" style="height: 35px;")
+    q-separator(dark)
+    q-list(dark)
       q-item(
         clickable
+        dark
         @click="gotoClinic"
       )
         q-item-section(avatar)
-          q-avatar(size="60px" style="outline: 1px solid lightgrey")
+          q-avatar(:style="{ 'margin-left': miniState ? '0px' : '-8px' }")
             img(:src="activeOrganization.picURL")
         q-item-section
-          q-item-label.ellipsis {{activeOrganization.name}}
-          q-item-label(caption).ellipsis-2-lines {{activeOrganization.description}}
+          q-item-label(lines="2") {{activeOrganization.name}}
+          q-item-label(caption).ellipsis {{activeOrganization.description}}
         q-item-section(side)
           switch-facility
       template(v-for="(nav, index) in navs")
         template(v-if="nav.type === 'nav-header'")
-          q-separator
+          q-separator(dark)
           q-item
             q-item-label(label).text-grey
               small.text-bold {{nav.name}}
@@ -56,7 +87,11 @@ q-layout(view="hHh LpR lFr" style="background: #fafafa")
 
 <script>
 import { computed, ref } from 'vue';
+import { formatDoctorName } from '@/utils';
+import { sdk } from '@/boot/mycure';
 import { usePmeNavRoutes } from '@/composables/navigation';
+import { useQuasarMixins } from '@/composables/quasar-mixins';
+import { useRouter } from 'vue-router';
 import { useUserStore } from '@/stores/current-user';
 import SwitchFacility from '@/components/commons/dialogs/SwitchFacility';
 
@@ -68,8 +103,14 @@ export default {
     const userStore = useUserStore();
     const leftDrawerOpen = ref(true);
     const navs = usePmeNavRoutes(['a']);
+    const miniState = ref(true);
     const organizations = computed(() => userStore.$state.userOrganizations);
     const activeOrganization = computed(() => userStore.$state.userActiveOrganization);
+    const currentUser = computed(() => userStore.$state.user);
+    const currentUserPicURL = computed(() => currentUser.value?.picURL);
+    const fullName = computed(() => formatDoctorName(currentUser.value));
+    const router = useRouter();
+    const { confirm } = useQuasarMixins();
 
     function gotoClinic () {
       console.warn('gotoClinic');
@@ -83,14 +124,25 @@ export default {
       leftDrawerOpen.value = !leftDrawerOpen.value;
     }
 
+    async function logOut () {
+      if (!await confirm({ message: 'Are you sure you want to logout?' })) return;
+      await sdk.service('auth').signout();
+      router.push({ name: 'login' });
+    };
+
     return {
       activeOrganization,
+      miniState,
       leftDrawerOpen,
       navs,
       organizations,
       gotoClinic,
       switchFacility,
       toggleLeftDrawer,
+      currentUser,
+      currentUserPicURL,
+      fullName,
+      logOut,
     };
   },
 };
